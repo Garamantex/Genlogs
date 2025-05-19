a3b5673501de64c5b95c2c9bb366fff10c07ea88
Orco <alexanderacosta2286@gmail.com>
2025-05-19 17:47:06 -0500

    Refactor server startup configuration by removing run.py and integrating server launch in main.py
    - Deleted the run.py file to streamline server execution.
    - Moved server startup logic to main.py, allowing for direct execution of the FastAPI application.
    - Updated the default port to 8000 and included necessary imports for Uvicorn.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,5 @@
+import os
+import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .routes import transport, general
@@ -16,4 +18,9 @@ app.add_middleware(
 
 # Incluir routers
 app.include_router(general.router)
-app.include_router(transport.router) 
+app.include_router(transport.router)
+
+# Arranque del servidor (necesario para Render.com)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
